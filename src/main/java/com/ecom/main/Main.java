211ae3e59c6880f7c113e72f3bbf8b1cffcec2db
Nayandeep <nayandeepsen@gmail.com>
2026-03-06 10:10:00 +0530

    added log4j logging in Main class

--- src/main/java/com/ecom/main/Main.java
+++ src/main/java/com/ecom/main/Main.java
@@ -1,18 +1,23 @@
 package com.ecom.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.ecom.ui.ConsoleHandler;
 
 public class Main {
 
+	private static final Logger logger = LogManager.getLogger(Main.class);
+
 	public static void main(String[] args) {
-		
-		System.out.println("======================================================");
-        System.out.println("             WELCOME TO E-COMMERCE SYSTEM             ");
-        System.out.println("======================================================");
-        System.out.println("Browse products, manage your cart and place orders.");
-        System.out.println();
-        
-        ConsoleHandler.showUI();
+
+		logger.info("======================================================");
+		logger.info("             WELCOME TO E-COMMERCE SYSTEM             ");
+		logger.info("======================================================");
+		logger.info("Browse products, manage your cart and place orders.");
+		System.out.println();
+
+		ConsoleHandler.showUI();
 
 	}
 
